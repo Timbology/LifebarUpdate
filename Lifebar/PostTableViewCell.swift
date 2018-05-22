b2ffab2a1e79d735585250ca2d79dff2d677f0d1
Timbology <i7679245@bournemouth.ac.uk>
2018-05-22 15:37:49 +0100

    Initial commit

---
+++ Lifebar/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Lifebar
+//
+//  Created by Timbology on 22/05/2018.
+//  Copyright © 2018 Timbology. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
